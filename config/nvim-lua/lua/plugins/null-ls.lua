fd054516882cf1adbac735a2b13b456cd4e7c5de
tomxkay <tomxkay@pm.me>
2022-10-15 23:10:51 -0400

    add source eslint null-ls

--- config/nvim-lua/lua/plugins/null-ls.lua
+++ config/nvim-lua/lua/plugins/null-ls.lua
@@ -1,13 +1,9 @@
 local null_ls = require("null-ls")
 
 null_ls.setup({
-  on_attach = function(client, bufnr)
-    if client.server_capabilities.documentFormattingProvider then
-      vim.cmd("nnoremap <silent><buffer> <Leader>f :lua vim.lsp.buf.formatting()<CR>")
-    end
-
-    if client.server_capabilities.documentRangeFormattingProvider then
-      vim.cmd("xnoremap <silent><buffer> <Leader>f :lua vim.lsp.buf.range_formatting({})<CR>")
-    end
-  end,
+  sources = {
+    null_ls.builtins.diagnostics.eslint_d.with({
+      diagnostics_format = '[eslint] #{m}\n(#{c})'
+    }),
+  },
 })
