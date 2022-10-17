4b9661f6984b9c77915577902817cdaf6eda8dd4
tomxkay <tomxkay@pm.me>
2022-10-16 22:09:32 -0400

    add null ls config

--- config/nvim-lua/lua/plugins/lsp.lua
+++ config/nvim-lua/lua/plugins/lsp.lua
@@ -39,6 +39,7 @@ local on_attach = function(client, bufnr)
   vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
   vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
   vim.keymap.set('n', '<space>f', vim.lsp.buf.format, bufopts)
+  vim.keymap.set('v', '<space>f', vim.lsp.buf.format, bufopts)
 end
 
 -- setup default lsp configs
--- config/nvim-lua/lua/plugins/null-ls.lua
+++ config/nvim-lua/lua/plugins/null-ls.lua
@@ -2,8 +2,10 @@ local null_ls = require("null-ls")
 
 null_ls.setup({
   sources = {
+    null_ls.builtins.formatting.rubocop,
+    null_ls.builtins.formatting.prettierd,
     null_ls.builtins.diagnostics.eslint_d.with({
-      diagnostics_format = '[eslint] #{m}\n(#{c})'
+      diagnostics_format = '[eslint_d] #{m}\n(#{c})'
     }),
   },
 })
