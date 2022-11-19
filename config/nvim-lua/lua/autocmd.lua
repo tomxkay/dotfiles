41cac4f6f75a258a63f0d999266c68918d1206cc
tomxkay <tomxkay@pm.me>
2022-11-19 00:17:16 -0500

    port autocmd to lua

--- config/nvim-lua/lua/autocmd.lua
+++ config/nvim-lua/lua/autocmd.lua
@@ -1,48 +1,42 @@
+local augroup = vim.api.nvim_create_augroup
+local autocmd = vim.api.nvim_create_autocmd
+
+local group = augroup("LuaAutoCmdGroup", { clear = true })
+
+autocmd(
+  "FileType",
+  {
+    desc = "Delete git buffer on exit",
+    pattern = { "gitcommit", "gitrebase", "gitconfig" },
+    command = "set bufhidden=delete",
+    group = group
+  })
+
+autocmd(
+  { "BufWritePost", "FileWritePost" },
+  {
+    desc = "Source lua on edit",
+    pattern = { "*.lua" },
+    command = "source % | redraw",
+    group = group
+  }
+)
+
+autocmd(
+  { "BufWritePre" },
+  {
+    desc = "Remove trailing whitespace on save",
+    pattern = { "*" },
+    command = ":%s/\\s\\+$//e",
+    group = group
+  }
+)
+
 vim.cmd [[
-  augroup DeleteGitBufferOnExit
-    autocmd!
-    autocmd FileType gitcommit,gitrebase,gitconfig set bufhidden=delete
-  augroup END
-
-  augroup Reload
-    autocmd!
-    autocmd BufWritePost,FileWritePost *.lua source % | redraw
-  augroup END
-
-  augroup Vimrc
-    autocmd!
-    autocmd BufWritePre * :%s/\s\+$//e " Remove trailing whitespace on save
-    autocmd FileType html,css EmmetInstall
-  augroup END
-
-  augroup SaveTrigger
-    autocmd!
-    "autocmd BufWrite * :Autoformat
-  augroup END
-
   augroup YankTrigger
     autocmd!
     autocmd TextYankPost * if v:event.operator is 'y' && v:event.regname is '' | execute 'OSCYankReg "' | endif
     autocmd TextYankPost * if v:event.operator is 'y' && v:event.regname is '+' | execute 'OSCYankReg "' | endif
     autocmd TextYankPost * if v:event.operator is 'y' && v:event.regname is '*' | execute 'OSCYankReg "' | endif
-  augroup END
-
-  augroup PersistView
-    autocmd!
-    " autocmd BufWinLeave * silent! mkview
-    " autocmd BufWinEnter * silent! loadview
-  augroup END
-
-  augroup Numbertoggle
-    autocmd!
-    " let blacklist = ['NvimTree']
-    " autocmd BufEnter,FocusGained * if index(blacklist, &ft) < 0 | set relativenumber | endif
-    " autocmd BufLeave,FocusLost   * if index(blacklist, &ft) < 0 | set norelativenumber | endif
-  augroup END
-
-  augroup VimFold
-    autocmd!
-    autocmd FileType vim setlocal foldmethod=marker
-    autocmd BufWinEnter * silent! :%foldopen!
   augroup END
 ]]
